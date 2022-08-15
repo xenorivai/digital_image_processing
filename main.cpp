edbb5e395d2e4a3ed944b7d7da4f565a4b3d9c35
Samyak W <wsamyak@gmail.com>
2022-08-15 18:48:38 +0530

    Refactor Code

--- main.cpp
+++ main.cpp
@@ -20,100 +20,179 @@
 namespace plt = matplotlibcpp;
 using namespace std;
 
+void grayscale_histogram_equalization(uint8_t* input, const int height, const int width, uint8_t* output);
+void grayscale_negative(uint8_t* input, const int height, const int width, uint8_t* output);
+void negative(uint8_t *input, const int height, const int width, uint8_t *output);
+
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		cerr << "usage : ./main [path-to-file]";
+		cerr << "usage : ./main [path-to-file]\n";
 		return 1;
 	}
 
-///Load image
+	///Load image
 	int width, height, bpp; //bpp - bytes per pixel
 	uint8_t* data = stbi_load(argv[1], &width, &height, &bpp, 3);
-	uint8_t* grayscale = new uint8_t[width*height] {0};
-	uint8_t* equalized = new uint8_t[width*height] {0};
-	if (data == NULL) {
-		cerr << "[STB_IMAGE] FAILED TO LOAD IMAGE\n";
-		return 1;
-	}
+	uint8_t* gray_output = new uint8_t[width * height] { 0 };
+	uint8_t* col_output = new uint8_t[width * height * 3] { 0 };
+	if (data == NULL) { cerr << "[STB_IMAGE] FAILED TO LOAD IMAGE\n"; return 1; }
 	cerr << "Image loaded!: " << argv[1] << " : " << width << " * " << height << endl;
 
+	cout << "Choose Operation: \n(0) Nothing\n(1) Negative\n(2) Grayscale Histogram Equalization\n";
+	int choice = 0;
+	cin >> choice;
+	if(choice == 0){//do nothing
 
-	vector<int> freq(256,0); // frequency table for grayscale image
-	vector<int> eq(256, 0);	 // frequency table for equalized image
+	}
+	
+	else if(choice == 1){//Image Negative
+		cout << "Grayscale(0) or Color(1) Negative : ";
+		cin >> choice;
+		if(choice == 0){//grayscale negative
+			uint8_t* grayscale = new uint8_t[width * height] { 0 };
+			///Convert To Grayscale
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					double r = static_cast<double>(data[(i * width + j) * 3 + 0]);
+					double g = static_cast<double>(data[(i * width + j) * 3 + 1]);
+					double b = static_cast<double>(data[(i * width + j) * 3 + 2]);
 
-///Stores mapping from intensity values to their PDF and CDF
-	map<int,pair<double,uint8_t>> df; // df[i] = ( pdf[i] , 255*cdf[i] );
-
-///Convert to grayscale
-	for(int i = 0; i < height; i++){
-		for(int j = 0; j < width; j++){
-
-			double r = static_cast<double>(data[(i*width + j)*3 + 0]);
-			double g = static_cast<double>(data[(i*width + j)*3 + 1]);
-			double b = static_cast<double>(data[(i*width + j)*3 + 2]);
-
-			grayscale[(i*width + j)] = (r + g + b)/3;
-
-			freq[grayscale[(i*width + j)]]++;
+					grayscale[i * width + j] = static_cast<uint8_t>((r + g + b) / 3);
+				}
+			}
+			grayscale_negative(grayscale,height,width,gray_output);
+			stbi_image_free(grayscale);
+		}
+		else if(choice == 1){//col negative
+			negative(data,height,width,col_output);
 		}
 	}
 
-	double cumulative = 0; /// stores cumuluative sum of pdf
+	else if(choice == 2){//grayscale histogram equalization
+		uint8_t* grayscale = new uint8_t[width * height] { 0 };
+		///Convert To Grayscale
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				double r = static_cast<double>(data[(i * width + j) * 3 + 0]);
+				double g = static_cast<double>(data[(i * width + j) * 3 + 1]);
+				double b = static_cast<double>(data[(i * width + j) * 3 + 2]);
 
-///Compute CDF and PDF of grayscale image
-	for(int i = 0; i < 256; i++){
-		auto y = double(freq[i])/(double(width*height));
-		cumulative += y;
-		df[i] = {y,floor(double(255)*cumulative)};
+				grayscale[i * width + j] = static_cast<uint8_t>((r + g + b) / 3);
+			}
+		}
+
+		///Histogram Equalization
+		grayscale_histogram_equalization(grayscale, height, width, gray_output);
+		stbi_image_free(grayscale);
 	}
 
-///Perform Hist Equalization
+	///Free up image data
+	stbi_image_free(data);
+	stbi_image_free(gray_output);
+
+	return 0;
+}
+
+
+void grayscale_negative(uint8_t* input, const int height, const int width, uint8_t* output){
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
-			
-			equalized[(i * width + j)] = df[grayscale[(i * width + j)]].second;
-
-			eq[equalized[(i * width + j)]]++;
+			output[i*width + j] = 255 - input[i*width + j];
 		}
 	}
-///Save Images
-	stbi_write_png("gray_scale.png",width,height,1,grayscale,width);
-	stbi_write_png("hist_eq.png",width,height,1,equalized,width);
 
-///Plot Hist and Images
-	plt::figure_size(960,480);
+	///Save Images
+	stbi_write_png("gray_scale.png", width, height, 1, input, width);
+	stbi_write_png("negative.png", width, height, 1, output, width);
+
+	///Plot Images
+	plt::figure_size(960, 480);
+	plt::title("Grayscale and Negative");
+	plt::subplot2grid(1, 2, 0, 0, 1, 1);
+	plt::imshow(input, height, width, 1);
+	plt::subplot2grid(1, 2, 0, 1, 1, 1);
+	plt::imshow(output, height, width, 1);
+
+	plt::show();
+}
+
+void negative(uint8_t *input, const int height, const int width, uint8_t *output){
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			output[(i*width + j)*3 + 0] = 255 - input[(i*width + j)*3 + 0];
+			output[(i*width + j)*3 + 1] = 255 - input[(i*width + j)*3 + 1];
+			output[(i*width + j)*3 + 2] = 255 - input[(i*width + j)*3 + 2];
+		}
+	}
+
+	///Save Images
+	stbi_write_png("input.png", width, height, 3, input, width*3);
+	stbi_write_png("negative.png", width, height, 3, output, width*3);
+
+	///Plot Images
+	plt::figure_size(960, 480);
+	plt::title("Image and Negative");
+	plt::subplot2grid(1, 2, 0, 0, 1, 1);
+	plt::imshow(input, height, width, 3);
+	plt::subplot2grid(1, 2, 0, 1, 1, 1);
+	plt::imshow(output, height, width, 3);
+
+	plt::show();
+}
+
+void grayscale_histogram_equalization(uint8_t* input, const int height, const int width, uint8_t* output)
+{
+	vector<int> freq(256, 0); /// frequency table for grayscale image
+	vector<int> eq(256, 0);	  /// frequency table for equalized image
+	double cumulative = 0;    /// stores cumuluative sum of pdf
+
+	///Stores mapping from intensity values to a pair of their PDF and CDF
+	map<int, pair<double, uint8_t>> df; // df[i] = ( pdf[i] , 255*cdf[i] );
+
+	///Build Frequency Table
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			freq[input[(i * width + j)]]++;
+		}
+	}
+
+	///Compute CDF and PDF of the grayscale image
+	for (int i = 0; i < 256; i++) {
+		auto y = double(freq[i]) / (double(width * height));
+		cumulative += y;
+		df[i] = { y, floor(double(255) * cumulative) };
+	}
+
+	///Perform Hist Equalization
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			output[(i * width + j)] = df[input[(i * width + j)]].second;
+			eq[output[(i * width + j)]]++;
+		}
+	}
+
+	///Save Images
+	stbi_write_png("gray_scale.png", width, height, 1, input, width);
+	stbi_write_png("hist_eq.png", width, height, 1, output, width);
+
+	///Plot Hist and Images
+	plt::figure_size(960, 480);
 	plt::title("GrayScale Image");
 	plt::xlabel("Grayscale Intensity");
 	plt::ylabel("Frequency");
-	plt::xlim(0,256);
+	plt::xlim(0, 256);
 
-	plt::subplot2grid(2,2,0,0,1,1);
-	plt::named_plot("Original Histogram",freq,"r");
+	plt::subplot2grid(2, 2, 0, 0, 1, 1);
+	plt::named_plot("Original Histogram", freq, "r");
 	plt::legend();
-	plt::subplot2grid(2,2,0,1,1,1);
-	plt::named_plot("Equalized Histogram",eq,"b");
+	plt::subplot2grid(2, 2, 0, 1, 1, 1);
+	plt::named_plot("Equalized Histogram", eq, "b");
 	plt::legend();
-	
-	plt::subplot2grid(2,2,1,0,1,1);
-	plt::imshow(grayscale,height,width,1);
-	plt::subplot2grid(2,2,1,1,1,1);
-	plt::imshow(equalized,height,width,1);
-	
+
+	plt::subplot2grid(2, 2, 1, 0, 1, 1);
+	plt::imshow(input, height, width, 1);
+	plt::subplot2grid(2, 2, 1, 1, 1, 1);
+	plt::imshow(output, height, width, 1);
+	///Display Results
 	plt::show();
-
-///Output statistis to stdout, pipe this to some file say "stats.txt"
-	cout << "Statistics:\n------------------------------\n";
-	cout << "\n\ni -- freq -- eq -- pdf -- 255*cdf\n\n\n";
-	for(int i = 0; i < 256; i++){
-		// cout << x.first << "\t-> " << eq[x.first] <<"\t,\t" <<x.second.first << "\t,\t" << x.second.second << endl;
-		printf("%d , \t %5d,\t %5d,\t %0.5f,\t %d\n",i,freq[i],eq[i],df[i].first,df[i].second);
-	}
-	cout << "\n------------------------------\n";
-
-///Free up image data
-	stbi_image_free(data);
-	stbi_image_free(grayscale);
-	stbi_image_free(equalized);
-
-	return 0;
 }
